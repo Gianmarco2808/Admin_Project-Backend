8e40a501da7a1ff0557f06d50bf13047071c0e64
Gean Vasquez <geanvasquezvi@gmail.com>
2024-07-11 11:50:19 -0500

    emails

--- src/emails/AuthEmail.ts
+++ src/emails/AuthEmail.ts
@@ -1,4 +1,6 @@
-import { transporter } from "../config/nodemailer"
+import { Resend } from "resend"
+import dotenv from 'dotenv'
+dotenv.config()
 
 interface IEmail {
      email: string
@@ -6,38 +8,44 @@ interface IEmail {
      token: string
 }
 
+const resend = new Resend(process.env.API_EMAIL)
+
 export class AuthEmail {
 
      static sendConfirmationEmail = async ( user : IEmail  ) => {
-          const info = await transporter.sendMail({
-               from: '<Admin> <admin@gmail.com>',
-               to: user.email,
-               subject: 'Admin - confirma tu cuenta',
-               text: 'Confirma tu cuenta',
+          const {data, error} = await resend.emails.send({
+               from: process.env.EMAIL_FROM,
+               to: [user.email],
+               subject: 'confirma tu cuenta',
                html: `<p>Hola: ${user.name}, has creado tu cuenta en Poject Admin, debes confirmar tu cuenta</p>
                     <p>Visita el siguiente enlace: </p>
-                    <a href="${process.env.FRONTEND_URL}/auth/confirm-account">Confirmar cuenta</a>
-                    <p>E ingresa el código: <b>${user.token}</b></p>
+                    <div class='text-center'>
+                         <a href="${process.env.FRONTEND_URL}/auth/confirm-account">Confirmar cuenta</a>
+                    </div>
+                    <p>ingresa el código: <b>${user.token}</b></p>
                     <p>Este token expira en 10 minutos</p>`
           })
-          console.log('Mensaje enviado', info.messageId);
-          
+          if (error) {
+               return console.error({error})
+          }
+          console.log('Mensaje enviado');     
      }
 
      static sendPasswordResetToken = async ( user : IEmail  ) => {
-          const info = await transporter.sendMail({
-               from: '<Admin> <admin@gmail.com>',
-               to: user.email,
-               subject: 'Admin - reestable tu password',
-               text: 'reestable tu password',
+          const {data, error} = await resend.emails.send({
+               from: process.env.EMAIL_FROM,
+               to: [user.email],
+               subject: 'Reestable tu password',
                html: `<p>Hola: ${user.name}, has solicitado reestablecer tu password.</p>
                     <p>Visita el siguiente enlace: </p>
                     <a href="${process.env.FRONTEND_URL}/auth/new-password">Reestablecer password</a>
                     <p>E ingresa el código: <b>${user.token}</b></p>
                     <p>Este token expira en 10 minutos</p>`
           })
-          console.log('Mensaje enviado', info.messageId);
-          
+          if (error) {
+               return console.error({error})
+          }
+          console.log('Mensaje enviado');
      }
 
 }
